e85ef99e0696190b5555798ccf1a94719c01682e
Beato Bongco <beatobongco@gmail.com>
2018-03-21 15:56:42 +0800

    Implement starting then showing of client on click of icon, allow insync status icon to change based on client status

--- support@insynchq.com/extension.js
+++ support@insynchq.com/extension.js
@@ -3,31 +3,77 @@ const Main = imports.ui.main
 const Util = imports.misc.util
 const Me = imports.misc.extensionUtils.getCurrentExtension()
 const Gio = imports.gi.Gio
+const GLib = imports.gi.GLib
 
-const insyncIcon = Gio.icon_new_for_string(Me.path + '/icons/normal.png')
-const icon = new St.Icon({gicon: insyncIcon, style_class: 'insync-icon'})
+const _onlineIcon = Gio.icon_new_for_string(Me.path + '/icons/normal.png')
+const _offlineIcon = Gio.icon_new_for_string(Me.path + '/icons/offline.png')
+const onlineIcon = new St.Icon({gicon: _onlineIcon, style_class: 'insync-icon'})
+const offlineIcon = new St.Icon({gicon: _offlineIcon, style_class: 'insync-icon'})
+const uid = _callCmd('id -u')
+const socketPath = '/tmp/insync' + uid + '.sock'
+
 let button
 
+function _callCmd (cmd) {
+  return GLib.spawn_command_line_sync(cmd)[1].toString().trim()
+}
+
+function _isRunning () {
+  // check if running
+  // if socket file exists
+  if (_callCmd('ls ' + socketPath) === socketPath) {
+    return true
+  }
+  return false
+}
+
+function _showInsync () {
+  var status = _callCmd('insync get_status')
+
+  if (status === 'UNLINKED' || !_isRunning()) {
+    return true
+  }
+
+  _callCmd('insync show')
+  button.set_child(onlineIcon)
+  return false
+}
+
 function _toggleInsync () {
-  Util.spawn(['/usr/bin/insync', 'toggle'])
+  if (!_isRunning()) {
+    _callCmd('insync start')
+    // timeout_add will keep on running until its function returns false
+    GLib.timeout_add(1000, null, _showInsync)
+  } else {
+    _callCmd('insync toggle')
+  }
 }
 
-function init() {
+function _checkStatus () {
+  if (_isRunning()) {
+    button.set_child(onlineIcon)
+  } else {
+    button.set_child(offlineIcon)
+  }
+  return true
+}
+
+function init () {
   button = new St.Bin({ style_class: 'panel-button',
-                        reactive: true,
-                        can_focus: true,
-                        x_fill: true,
-                        y_fill: true,
-                        track_hover: true })
+    reactive: true,
+    can_focus: true,
+    x_fill: true,
+    y_fill: true,
+    track_hover: true })
 
-  button.set_child(icon)
   button.connect('button-press-event', _toggleInsync)
+  GLib.timeout_add(1000, null, _checkStatus)
 }
 
-function enable() {
+function enable () {
   Main.panel._rightBox.insert_child_at_index(button, 0)
 }
 
-function disable() {
+function disable () {
   Main.panel._rightBox.remove_child(button)
 }
